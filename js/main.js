7515285794df6e4cddb4c3e94205726b2f526a98
Imari Joy Borda <imari.borda2018@gmail.com>
2022-04-30 22:14:31 +0800

    Refactor

--- js/main.js
+++ js/main.js
@@ -17,15 +17,15 @@ class Game {
         this.winner = undefined
     }
 
-    placeMove(piece) {
-        let col = +piece.id.split('-')[0]
-        let row = +piece.id.split('-')[1]
+    placeMove(move) {
+        let col = +move.id.split('-')[0]
+        let row = +move.id.split('-')[1]
         this.moves[col][row] = this.turn
     }
 
-    isValidMove(piece) {
-        let col = +piece.id.split('-')[0]
-        let row = +piece.id.split('-')[1]
+    isValidMove(move) {
+        let col = +move.id.split('-')[0]
+        let row = +move.id.split('-')[1]
         if (this.moves[col][row] === 0) {
             return true
         } else {
@@ -37,38 +37,39 @@ class Game {
         this.turn = this.turn === 1 ? 2 : 1
     }
 
-    checkWinner() {
-        let wins = [[1, 0, 0, 1, 0, 0, 1, 0, 0],
-                    [0, 0, 1, 0, 0, 1, 0, 0, 1],
-                    [0, 1, 0, 0, 1, 0, 0, 1, 0],
-                    [1, 1, 1, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 1, 1, 1, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 1, 1, 1],
-                    [1, 0, 0, 0, 1, 0, 0, 0, 1],
-                    [0, 0, 1, 0, 1, 0, 1, 0, 0]]
-        let isP1win = wins.some(win => isWinningPosition([].concat(...this.moves).map(a => a === 1 ? 1 : 0), win))
-        let isP2win = wins.some(win => isWinningPosition([].concat(...this.moves).map(a => a === 2 ? 1 : 0), win))
+    isGameEnd() {
+        let winPositions = [[1, 0, 0, 1, 0, 0, 1, 0, 0],
+                            [0, 0, 1, 0, 0, 1, 0, 0, 1],
+                            [0, 1, 0, 0, 1, 0, 0, 1, 0],
+                            [1, 1, 1, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 1, 1, 1, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 1, 1, 1],
+                            [1, 0, 0, 0, 1, 0, 0, 0, 1],
+                            [0, 0, 1, 0, 1, 0, 1, 0, 0]]
+        let isP1win = winPositions.some(pos => isWinningPosition([].concat(...this.moves).map(a => a === 1 ? 1 : 0), pos))
+        let isP2win = winPositions.some(pos => isWinningPosition([].concat(...this.moves).map(a => a === 2 ? 1 : 0), pos))
         let isAllFilled = [].concat(...this.moves).every(a => a !== 0)
-        if (isP1win === true && isP2win === true) {
-            return 'Something is wrong'  
-        } else if (isP1win === false && isP2win === false && isAllFilled === true) {
+        if (isP1win === false && isP2win === false && isAllFilled === true) {
             this.winner = 'Tie'
             this.ties += 1
-        } else if (isP1win === false && isP2win === false) {
-            return 'No winner yet'
+            return true
         } else if (isP1win === true && isP2win === false) {
             this.winner = 'Player 1'
             this.p1wins += 1
-        } else  {
+            return true
+        } else if (isP1win === false && isP2win === true) {
             this.winner = 'Player 2'
             this.p2wins += 1
+            return true
+        } else {
+            return false
         }
         function isWinningPosition(arr, winPositions) {
             return arr.map((a, i) => a === winPositions[i] ? true : winPositions[i] ? false : true).every(a => a === true)
         }
     }
 
-    newGame() {
+    startNewGame() {
         this.turn = 1
         this.moves = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         this.winner = undefined
@@ -80,33 +81,31 @@ let game = new Game
 // Listen for placements
 Array.from( document.querySelectorAll('.box') ).forEach(a => {
     a.addEventListener('click', function (e) {
-        let piece = e.target.querySelector('.piece')
-        if (game.isValidMove(piece)) {
-            game.placeMove(piece)
-            piece.classList.add(`p${game.turn}`)
-            game.checkWinner()
-            if (game.winner === 'Player 1') {
-                document.querySelector('#p1Score').innerHTML = game.p1wins
-                game.newGame()
+        // Get move
+        let move = e.target.querySelector('.piece')
+        // Check if move is valid
+        if (game.isValidMove(move)) {
+            // Place move on board
+            game.placeMove(move)
+            move.classList.add(`p${game.turn}`)
+            // Chek if game should be ended
+            if (game.isGameEnd()) {
+                if (game.winner === 'Player 1') {
+                    document.querySelector('#p1Score').innerHTML = game.p1wins     
+                }
+                if (game.winner === 'Player 2') {
+                    document.querySelector('#p2Score').innerHTML = game.p2wins
+                }
+                if (game.winner === 'Tie') {
+                    document.querySelector('#ties').innerHTML = game.ties
+                }
+                game.startNewGame()
                 Array.from( document.querySelectorAll('.piece')).forEach(a => {
                     a.className = 'piece'
                 }) 
+            } else {
+                game.changeTurn()
             }
-            if (game.winner === 'Player 2') {
-                document.querySelector('#p2Score').innerHTML = game.p2wins
-                game.newGame()
-                Array.from( document.querySelectorAll('.piece')).forEach(a => {
-                    a.className = 'piece'
-                }) 
-            }
-            if (game.winner === 'Tie') {
-                document.querySelector('#ties').innerHTML = game.ties
-                game.newGame()
-                Array.from( document.querySelectorAll('.piece')).forEach(a => {
-                    a.className = 'piece'
-                }) 
-            }
-            game.changeTurn()
         }
     }, false)
 })
